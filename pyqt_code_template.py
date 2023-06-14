5d25d4581ba25055bc724a1e11ea83637a66d895
promto-c <prommm01@gmail.com>
2023-06-15 00:54:44 +0700

    update docstring style

--- pyqt_code_template.py
+++ pyqt_code_template.py
@@ -2,24 +2,24 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class MyWidget(QtWidgets.QWidget):
-    ''' A PyQt5 widget that performs some task.
+    """A PyQt5 widget that performs some task.
 
     Attributes:
         some_arg (Any): An argument that will be used in the widget.
         some_value (int): A value used in the widget.
-    '''
+    """
     # Initialization and Setup
     # ------------------------
     def __init__(self, parent=None, some_arg=None):
-        ''' Initialize the widget and set up the UI, signal connections, and icon.
+        """Initialize the widget and set up the UI, signal connections, and icon.
             Args:
                 parent (QtWidgets.QWidget): The parent widget.
                 some_arg (Any): An argument that will be used in the widget.
-        '''
+        """
         # Initialize the super class
-        super(MyWidget, self).__init__(parent)
+        super().__init__(parent)
 
-        # Save the argument
+        # Store the arguments
         self.some_arg = some_arg
 
         # Set up the initial values
@@ -32,45 +32,60 @@ class MyWidget(QtWidgets.QWidget):
         self._setup_icon()
 
     def _setup_initial_values(self):
-        ''' Set up the initial values for the widget.
-        '''
+        """Set up the initial values for the widget.
+        """
+        # Attributes
+        # ------------------
         self.some_value = 0
 
+        # Private Attributes
+        # ------------------
+        self._some_private = 0
+
     def _setup_ui(self):
-        ''' Set up the UI for the widget, including creating widgets and layouts.
-        '''
+        """Set up the UI for the widget, including creating widgets and layouts.
+        """
         # Create widgets and layouts here
         pass
         # Set the layout for the widget
         # self.setLayout(layout)
 
     def _setup_signal_connections(self):
-        ''' Set up signal connections between widgets and slots.
-        '''
+        """Set up signal connections between widgets and slots.
+        """
         # Connect signals to slots here
         pass
 
     def _setup_icon(self):
-        ''' Set the icon for the widget.
-        '''
+        """Set the icon for the widget.
+        """
         # Set the icon for the widget here
         pass
+
+    # Private Methods
+    # ---------------
 
     # Extended Methods
     # ----------------
     def some_function(self):
-        ''' Slot for a signal connection.
-        '''
+        """Slot for a signal connection.
+        """
         pass
+
+    # Special Methods
+    # ---------------
 
     # Event Handling or Override Methods
     # ----------------------------------
-    def keyPressEvent(self, event):
-        super(MyWidget, self).keyPressEvent(event)
+    def keyPressEvent(self, event: QtGui.QKeyEvent):
+        """Handle key press events.
+        """
+        # Handle key press events here
+        super().keyPressEvent(event)
 
 def main():
-    ''' Create the application and main window, and show the widget.
-    '''
+    """Create the application and main window, and show the widget.
+    """
     # Create the application and the main window
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
